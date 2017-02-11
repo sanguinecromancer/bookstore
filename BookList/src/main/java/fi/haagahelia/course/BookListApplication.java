1c86553df1dbe2d1f6e55937a656e85a75c8a8f6
Zeynep <zeynep.aykal@ibm.com>
2017-02-11 18:08:09 +0200

    first commit

---
+++ BookList/src/main/java/fi/haagahelia/course/BookListApplication.java
@@ -0,0 +1,36 @@
+package fi.haagahelia.course;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import fi.haagahelia.course.domain.Book;
+import fi.haagahelia.course.domain.BookRepository;
+
+@SpringBootApplication
+public class BookListApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookListApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookListApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository repository) {
+		return (args) -> {
+			log.info("save a couple of books");
+			repository.save(new Book("name of the rose", "3454", 3, 3, 1));
+			repository.save(new Book("sdfd", "fgf", 5, 2, 1));	
+			
+			log.info("fetch all books");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+
+		};
+	}
+}
---
+++ BookList/src/main/java/fi/haagahelia/course/domain/Book.java
@@ -0,0 +1,99 @@
+package fi.haagahelia.course.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	 long id;
+	 String title;
+	 String author;
+	 long date;
+	 long isbn;
+	 Double price;
+	 
+	 public Book() {
+		 
+	 }
+	 
+	 public Book (String title, String author, long date, long isbn, double price)
+	 {
+		 super();
+		 this.title = title;
+		 this.author = author;
+		 this.date = date;
+		 this.isbn = isbn;
+		 this.price = price;
+	 }
+		
+	 public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public long getDate() {
+		return date;
+	}
+
+	public void setDate(long date) {
+		this.date = date;
+	}
+
+	public long getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book title=" + title + ", author=" + author + ", date=" + date
+				+ ", isbn=" + isbn + ", price=" + price ;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
